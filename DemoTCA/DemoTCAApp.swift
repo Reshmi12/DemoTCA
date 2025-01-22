965aa3ca7e2af6323d87486711a4f3339cb30361
Reshmi <reshmimajumder1@gmail.com>
2025-01-22 09:39:57 -0500

    Initial Commit

---
+++ DemoTCA/DemoTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTCAApp.swift
+//  DemoTCA
+//
+//  Created by Reshmi Majumder on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoTCATests/DemoTCATests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoTCATests.swift
+//  DemoTCATests
+//
+//  Created by Reshmi Majumder on 22/01/25.
+//
+
+import Testing
+@testable import DemoTCA
+
+struct DemoTCATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
